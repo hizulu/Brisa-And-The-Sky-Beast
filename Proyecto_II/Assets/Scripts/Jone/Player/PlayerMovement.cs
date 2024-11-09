1847fee9455963ce02743a8ad551a5ffea439674
Jonetxu10 <jse1007@alu.ubu.es>
2024-11-09 15:45:02 +0100

    Movimiento_v2
    Salto
    Controles de consola

--- Proyecto_II/Assets/Scripts/Jone/Player/PlayerMovement.cs
+++ Proyecto_II/Assets/Scripts/Jone/Player/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,19 +9,36 @@ using UnityEngine.InputSystem;
  * FECHA: 09/11/2024
  * DESCRIPCIÓN: Script base que se encarga del movimiento del personaje jugable usando el New Input System
  * VERSIÓN: 1.0 movimiento base con W/A/S/D
+ *          2.0 salto
  */
 
 public class PlayerMovement : MonoBehaviour
 {
     PlayerInput playerInput;
-    InputAction walkAction;
+    [SerializeField] InputActionReference walkAction;
+    [SerializeField] InputActionReference jumpAction;
+
+    Rigidbody rb;
+
     [SerializeField] float baseSpeed = 5f;
     [SerializeField] float movementSpeedMultiplier = 1f;
-    
+    [SerializeField] float jumpForce = 5f;
+    private bool isGrounded = true;
+
+    private void OnEnable()
+    {
+        jumpAction.action.started += Jump;
+    }
+
+    private void OnDisable()
+    {
+        jumpAction.action.started -= Jump;
+    }
+
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();
-        walkAction = playerInput.actions.FindAction("Walk");
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
@@ -38,7 +56,20 @@ public class PlayerMovement : MonoBehaviour
      */
     void PlayerWalk()
     {
-        Vector2 direction = walkAction.ReadValue<Vector2>();
+        Vector2 direction = walkAction.action.ReadValue<Vector2>();
         transform.position += new Vector3(direction.x, 0, direction.y) * movementSpeedMultiplier * baseSpeed * Time.deltaTime;
+    }
+
+    /* NOMBRE MÉTODO: Jump
+     * AUTOR: Jone Sainz Egea
+     * FECHA: 09/11/2024
+     * DESCRIPCIÓN: añade impulso de salto cuando se llama a la acción de salto
+     * @param: contexto de salto
+     * @return: - 
+     */
+    private void Jump(InputAction.CallbackContext context)
+    {
+        if (isGrounded)
+            rb.AddForce(new Vector3(0, 1, 0) * jumpForce, ForceMode.Impulse);
     }
 }
