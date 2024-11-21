53004453efbe3f73b7a1562259131dc6ec66d864
Jonetxu10 <jse1007@alu.ubu.es>
2024-11-21 11:48:39 +0100

    MaquinaDeEstadosBase
    Maquina de estados de movimiento muy básica
    Estado idle
    Estado walk
    Estado run
    Estado crouched

---
+++ Proyecto_II/Assets/Scripts/Jone/Player/PlayerStateMachine/StateMachine/PlayerState.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState
+{
+    // Clase que define cómo serán los estados del jugador
+
+    #region Movement Variables
+    Rigidbody rb;
+    public Animator anim { get; set; } // Public? --> DUDA
+
+    protected float baseSpeed = 5f;
+    protected float speedMultiplier = 1f;
+    // protected float rotationSpeed = 15f;
+
+    protected private Transform camTransform;
+    #endregion
+
+    protected Player player;
+    protected PlayerStateMachine playerStateMachine;
+    public PlayerState(Player player, PlayerStateMachine playerStateMachine)
+    {
+        this.player = player;
+        this.playerStateMachine = playerStateMachine;
+    }
+
+    public PlayerState(Player player, PlayerStateMachine playerStateMachine, float baseSpeed, float speedMultiplier)
+    {
+        this.player = player;
+        this.playerStateMachine = playerStateMachine;
+        this.baseSpeed = baseSpeed;
+        this.speedMultiplier = speedMultiplier;
+    }
+
+
+    public virtual void EnterState() { }
+    public virtual void ExitState() { }
+    public virtual void FrameUpdate() { }
+    public virtual void FixedUpdate() { }
+
+}
