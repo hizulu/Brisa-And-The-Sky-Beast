1783773135ea906e497e00f98561ba20763a712e
SaraYueMadrugaMartin <smm1032@alu.ubu.es>
2024-11-30 14:19:22 +0100

    Arreglos e implementación nueva
    Se ha arreglado lo de correr y andar (no solaparse la animación).
    
    Se ha implementado sonidos, pero no funciona bien cuando pasa de andar a correr, no detecta que cambias de estado y no reproduce el sonido de "correr", sigue la de "andar".
    
    RECORDAR: meter la música de fondo (se ha quitado porque era un archivo muy grande y sino no se podía hacer merge a main).

--- Proyecto_II/Assets/Scripts/Jone/Player/PlayerMovement.cs
+++ Proyecto_II/Assets/Scripts/Jone/Player/PlayerMovement.cs
@@ -62,6 +62,8 @@ public class PlayerMovement : MonoBehaviour
     private bool isAttackTutoActive = true;
     #endregion
 
+    AudioManager audioManager;
+
     private void OnEnable()
     {
         jumpAction.action.started += Jump;
@@ -72,6 +74,11 @@ public class PlayerMovement : MonoBehaviour
     {
         jumpAction.action.started -= Jump;
         attackAction.action.started -= Attack;
+    }
+
+    private void Awake()
+    {
+        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
     }
 
     void Start()
@@ -117,11 +124,13 @@ public class PlayerMovement : MonoBehaviour
             newPosition = Quaternion.AngleAxis(camTransform.rotation.eulerAngles.y, Vector3.up) * newPosition;
             Quaternion targetRotation = Quaternion.LookRotation(newPosition);
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            audioManager.PlaySFX(audioManager.walk);
             //Debug.Log("Estás andando" + " " + currentSpeed);
         }
         else
         {
             anim.SetBool("isWalking", false);
+            audioManager.StopSFX();
         }
 
         transform.position += newPosition * movementSpeedMultiplier * currentSpeed * Time.deltaTime;
@@ -132,16 +141,18 @@ public class PlayerMovement : MonoBehaviour
         if (isRunTutoActive)
             return;
 
-        if (runAction.action.IsPressed())
+        if (runAction.action.IsPressed() && walkAction.action.ReadValue<Vector2>() != Vector2.zero)
         {
             anim.SetBool("isRunning", true);
             currentSpeed = runSpeed;
+            audioManager.PlaySFX(audioManager.run);
             //Debug.Log("Estás corriendo" + " " + currentSpeed);
         }
         else
         {
             currentSpeed = baseSpeed;
             anim.SetBool("isRunning", false);
+            audioManager.StopSFX();
         }
     }
 
@@ -258,7 +269,7 @@ public class PlayerMovement : MonoBehaviour
         tutorialPanel.SetActive(true);
         textTutorial.text = "Para correr utiliza las teclas W A S D y mantén pulsado SHIFT izquierdo.";
 
-        yield return new WaitUntil(() => runAction.action.IsPressed()); // El tutorial de correr no se desactiva hasta que no se realiza la acción de correr (pulsar LEFT SHIFT).
+        yield return new WaitUntil(() => runAction.action.IsPressed() && walkAction.action.ReadValue<Vector2>() != Vector2.zero); // El tutorial de correr no se desactiva hasta que no se realiza la acción de correr (pulsar LEFT SHIFT).
         StartCoroutine(CheckTutorial(tutorialPanel.GetComponent<Image>().color, Color.green, 0.5f));
         isRunTutoActive = false;
         StopCoroutine(WalkTutorial());
