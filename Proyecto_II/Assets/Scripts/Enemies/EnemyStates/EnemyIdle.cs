3f215fc3b3f7f1f66ab7cb0fefa4fbf00e707467
Jone <jse1007@alu.ubu.es>
2025-03-18 10:27:50 +0100

    FirstApproachAtEnemyStateMachine
    Trying to implement the state machine with scriptable objects

---
+++ Proyecto_II/Assets/Scripts/Enemies/EnemyStates/EnemyIdle.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyIdle : EnemyStateTemplate
+{
+    public EnemyIdle(EnemyStateMachine _stateMachine) : base(_stateMachine)
+    {
+    }
+
+    public override void Enter()
+    {
+
+    }
+
+    public override void Exit() { }
+
+    public override void OnTriggerEnter(Collider collider) { }
+
+    public override void OnTriggerExit(Collider collider) { }
+
+    public override void UpdateLogic() { }
+
+    public override void UpdatePhysics() { }
+    protected override void MoveEnemy()
+    {
+
+    }
+}
