3282a1e857d4f71589e714ca74d294e7d23c3cdb
Jonetxu10 <jse1007@alu.ubu.es>
2025-03-25 03:51:47 +0100

    FirstSlimeAndSoldierPatrolChanges
    Patrol point to point and patrol random wander behaviors added

--- Proyecto_II/Assets/Scripts/Enemies/ScriptableObjects/PatrolBehaviors/EnemyPatrolPointToPoint.cs
+++ Proyecto_II/Assets/Scripts/Enemies/ScriptableObjects/PatrolBehaviors/EnemyPatrolPointToPoint.cs
@@ -5,53 +5,54 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Patrol-Point to Point", menuName = "Enemy Logic/Patrol Logic/Point to Point")]
 public class EnemyPatrolPointToPoint : EnemyPatrolSOBase
 {
-    //[SerializeField] private float PointToPointMovementSpeed = 1f;
+    [SerializeField] private float PointToPointMovementSpeed = 1f;
 
-    //private Transform _point1;
-    //private Transform _point2;
+    private Transform _point1;
+    private Transform _point2;
 
-    //private Vector3 _targetPos;
-    //private Vector3 _direction;
+    private Vector3 _targetPos;
+    private Vector3 _direction;
 
-    //private bool _point;
+    private bool _point;
 
     public override void DoEnterLogic()
     {
         base.DoEnterLogic();
         Debug.Log("Has entrado en estado de PatrolPointToPoint");
-        //Transform parent = transform.parent;
+        Transform parent = transform.parent;
 
-        //// Encuentra los objetos hijo por su nombre
-        //_point1 = parent.Find("Point1");
-        //_point2 = parent.Find("Point2");
+        // Encuentra los objetos hijo por su nombre
+        GameObject _point1 = GameObject.Find("Point1");
+        GameObject _point2 = GameObject.Find("Point2");
 
-        //if (_point1 == null)
-        //    Debug.LogError("Point1 no encontrado como hijo de " + parent.name);
-        //if (_point2 == null)
-        //    Debug.LogError("Point2 no encontrado como hijo de " + parent.name);
+        if (_point1 == null)
+            Debug.LogError("Point1 no encontrado como hijo de " + parent.name);
+        if (_point2 == null)
+            Debug.LogError("Point2 no encontrado como hijo de " + parent.name);
 
         //_targetPos = _point1.position;
-        //_point = false;
+        _point = false;
     }
 
     public override void DoExitLogic()
     {
         base.DoExitLogic();
+        Debug.Log("Has salido del estado Patrol - Point To Point");
     }
 
     public override void DoFrameUpdateLogic()
     {
         base.DoFrameUpdateLogic();
 
-        //_direction = (_targetPos - enemy.transform.position).normalized;
+        _direction = (_targetPos - enemy.transform.position).normalized;
 
-        ////enemy.MoveEnemy(_direction * PointToPointMovementSpeed);
+        enemy.MoveEnemy(_direction * PointToPointMovementSpeed);
 
-        //if ((enemy.transform.position - _targetPos).sqrMagnitude < 0.01f)
-        //{
-        //    _targetPos = GetNewTarget();
-        //    _point = !_point;
-        //}
+        if ((enemy.transform.position - _targetPos).sqrMagnitude < 0.01f)
+        {
+            _targetPos = GetNewTarget();
+            _point = !_point;
+        }
     }
 
     public override void DoPhysicsLogic()
@@ -69,11 +70,11 @@ public class EnemyPatrolPointToPoint : EnemyPatrolSOBase
         base.ResetValues();
     }
 
-    //private Vector3 GetNewTarget()
-    //{
-    //    if (_point)
-    //        return _point1.position;
-    //    else
-    //        return _point2.position;
-    //}
+    private Vector3 GetNewTarget()
+    {
+        if (_point)
+            return _point1.position;
+        else
+            return _point2.position;
+    }
 }
---
+++ Proyecto_II/Assets/Scripts/Enemies/ScriptableObjects/PatrolBehaviors/EnemyPatrolRandomWander.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Jone Sainz Egea
+// 24/03/2025
+[CreateAssetMenu(fileName = "Patrol-Random Wander", menuName = "Enemy Logic/Patrol Logic/Random Wander")]
+public class EnemyPatrolRandomWander : EnemyPatrolSOBase
+{
+    [SerializeField] private float minRandomRadius = 5f;
+    [SerializeField] private float maxRandomRadius = 12f;
+    [SerializeField] private float randomMovementSpeed = 3f;
+
+    private Vector3 targetPos;
+    private Vector3 direction;
+
+    public override void DoEnterLogic()
+    {
+        base.DoEnterLogic();
+
+        targetPos = GetRandomPointInRingAroundEnemy();
+    }
+
+    public override void DoExitLogic()
+    {
+        base.DoExitLogic();
+    }
+
+    public override void DoFrameUpdateLogic()
+    {
+        base.DoFrameUpdateLogic();
+
+        direction = (targetPos - enemy.transform.position).normalized;
+
+        enemy.MoveEnemy(direction * randomMovementSpeed);
+
+        if ((enemy.transform.position - targetPos).sqrMagnitude < 0.01f)
+        {
+            enemy.doIdle = true;
+            enemy.doPatrol = false;
+        }
+    }
+
+    public override void DoPhysicsLogic()
+    {
+        base.DoPhysicsLogic();
+    }
+
+    public override void Initialize(GameObject gameObject, Enemy enemy)
+    {
+        base.Initialize(gameObject, enemy);
+    }
+
+    public override void ResetValues()
+    {
+        base.ResetValues();
+    }
+
+    private Vector3 GetRandomPointInRingAroundEnemy()
+    {
+        Debug.Log("Nuevo punto en la rosca");
+
+        float randomRadius = Random.Range(minRandomRadius, maxRandomRadius);
+        float randomAngle = Random.Range(0f, Mathf.PI * 2); // Ángulo aleatorio en radianes
+
+        float offsetX = Mathf.Cos(randomAngle) * randomRadius;
+        float offsetZ = Mathf.Sin(randomAngle) * randomRadius;
+
+        return new Vector3(enemy.transform.position.x + offsetX, enemy.transform.position.y, enemy.transform.position.z + offsetZ);
+    }
+}
