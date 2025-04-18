d985c55acf888d6f9cc41be7eaf95b798d6975cd
Jonetxu10 <jse1007@alu.ubu.es>
2025-04-18 20:52:42 +0200

    TeleportsForDebugging

--- Proyecto_II/Assets/zz_ToDelete/ScriptsNoSirven/TEMPTeleportToPosition.cs
+++ Proyecto_II/Assets/zz_ToDelete/ScriptsNoSirven/TEMPTeleportToPosition.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class TEMPTeleportToPosition : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Transform teleportHere;
+    private Transform player;
+
+    [SerializeField] GameObject camGO;
+    private CameraFade cam;
+
+    private void Start()
     {
-        
+        cam = camGO.GetComponent<CameraFade>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            // TODO: don't allow Brisa to move!!!!
+            player = other.transform;
+            StartCoroutine(SendPlayerToPosition(player));
+        }
+    }
+
+    IEnumerator SendPlayerToPosition(Transform _playerTransform)
+    {
+        // TODO: some animation
+        cam.DoFadeInOut();
+        yield return new WaitForSeconds(1f);
+        _playerTransform.position = teleportHere.position;
+        yield return new WaitForSeconds(0.5f);
+        cam.DoFadeInOut();
     }
 }
