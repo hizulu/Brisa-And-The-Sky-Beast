1e28c75f4b85efb5137c8119bbca10de9aa724f4
Jonetxu10 <jse1007@alu.ubu.es>
2025-04-29 03:23:40 +0200

    BeastCombatState
    Modified Beast Testing Scene
    Beast enters combat state

--- Proyecto_II/Assets/Scripts/Jone/Beast/BeastStates/BeastCombatState.cs
+++ Proyecto_II/Assets/Scripts/Jone/Beast/BeastStates/BeastCombatState.cs
@@ -1,3 +1,4 @@
+using BehaviorTree;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,16 +7,66 @@ using UnityEngine;
 // 19/04/2025
 public class BeastCombatState : BeastState
 {
+    private Node behaviorTree;
+    private Blackboard blackboard;
+
     public override void OnEnter(Beast beast)
     {
-    
+        Debug.Log("Beast has entered combat state");
+
+        blackboard = beast.blackboard;
+
+        // Activamos las flag en el Blackboard
+        blackboard.SetValue("isConstrained", false);
+        blackboard.SetValue("lookForTarget", true);
+        blackboard.SetValue("isCoroutineActive", false);
+
+        // Creamos el árbol de comportamiento libre
+        behaviorTree = SetupCombatBehaviorTree(beast);
     }
     public override void OnUpdate(Beast beast)
     {
-    
+        if (behaviorTree != null)
+            behaviorTree.Evaluate();
     }
     public override void OnExit(Beast beast)
     {
-    
+        Debug.Log("Beast has leaved combat state");
+    }
+
+    private Node SetupCombatBehaviorTree(Beast beast)
+    {
+        return new Sequence(new List<Node>
+        {
+            // new GetCombatTarget()
+            // new GoToCombatTarget()
+            // new AttackCombatTarget()
+            // new CooldownForCombat()
+
+            //new CheckFlag(blackboard, "goToPlayer",
+            //    new GoToPlayer(blackboard, beast, playerTransform, beast.arrivalThreshold)),
+            //new CheckFlag(blackboard, "reachedPlayer",
+            //    new WaitForOrder(blackboard, beast, playerTransform, beast.interactionThreshold, 8f)),
+            //new CheckFlag(blackboard, "menuOpened",
+            //    new Selector (new List<Node>
+            //    {
+            //        new CheckFlag(blackboard, "isOptionPet",
+            //            new PetBeast(blackboard, beast)),
+            //        new CheckFlag(blackboard, "isOptionHeal",
+            //            new HealBeast(blackboard, beast, beast.healingAmount)),
+            //        new CheckFlag(blackboard, "isOptionAttack",
+            //            new TransitionToBeastState(beast, new BeastCombatState())),
+            //        new CheckFlag(blackboard, "isOptionMount",
+            //            new TransitionToBeastState(beast, new BeastMountedState())),
+            //        new CheckFlag(blackboard, "isOptionAction",
+            //            new SpecificActions(blackboard, beast)),
+            //        new AlwaysTrue()
+            //    })),
+            //new Sequence(new List<Node>
+            //{
+            //    new IdleBehavior(blackboard, beast, 40f, 20f),
+            //    new TransitionToBeastState(beast, new BeastFreeState())
+            //})
+        });
     }
 }
