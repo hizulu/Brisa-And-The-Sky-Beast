c35d5d32a1e94c01757b861b44ac1e28cc1b84c9
Jonetxu10 <jse1007@alu.ubu.es>
2025-05-21 06:17:35 +0200

    MainMenuRework

---
+++ Proyecto_II/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using static GameManager;
+
+// Lucía García López, Jone Sainz Egea
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject creditosMenu;
+    [SerializeField] private GameObject seguroMenu;
+
+    private void Start()
+    {
+        mainMenu.SetActive(true);
+        creditosMenu.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (creditosMenu.activeSelf || seguroMenu.activeSelf)
+            {
+                Volver();
+            }
+            else
+            {
+                EstasSeguro();
+            }
+        }
+    }
+
+    public void PlayGame()
+    {
+        EventsManager.CleanAllEvents();
+
+        GameSession.IsNewGame = true;
+
+        GameManager.Instance.LoadScene("01_OpeningCinematic");
+    }
+
+    public void ContinueGame()
+    {
+        GameSession.IsNewGame = false;
+        GameManager.Instance.LoadScene("02_TheHollow"); //TODO: guardar nombre de escena
+    }
+
+    public void Creditos()
+    {
+        mainMenu.SetActive(false);
+        creditosMenu.SetActive(true);
+    }
+
+    public void Volver()
+    {
+        mainMenu.SetActive(true);
+        creditosMenu.SetActive(false);
+        seguroMenu.SetActive(false);
+    }
+
+    public void EstasSeguro()
+    {
+        seguroMenu.SetActive(true);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Has salido del juego");
+    }
+}
