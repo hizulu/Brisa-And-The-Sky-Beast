6bf3ad57b9ae9e7405386c54bcb9433359e87a0d
hizulu <lucia.gl.vega@gmail.com>
2025-05-23 00:59:45 +0200

    Cabecera CinematicsVolumeController

--- Proyecto_II/Assets/Scripts/UI/Settings/CinematicsVolumeController.cs
+++ Proyecto_II/Assets/Scripts/UI/Settings/CinematicsVolumeController.cs
@@ -1,6 +1,14 @@
 using UnityEngine;
 using UnityEngine.Video;
 
+
+/*NOMBRE CLASE: CinematicsVolumeController
+ * AUTOR: Lucía García López
+ * FECHA: 22/05/2025
+ * DESCRIPCIÓN: Script que gestiona el volumen de las cinemáticas.
+ * VERSIÓN: 1.0 El volumen de las cinemáticas se ajusta en función del volumen general y el volumen de las cinemáticas.
+ */
+
 public class CinematicsVolumeController : MonoBehaviour
 {
     public VideoPlayer cinematicVideoPlayer;
